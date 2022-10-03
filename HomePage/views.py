bde875dfee8a57f4d5ef405366ef8e479e9a27cf
Simon <ochotnicky.simon1@gmail.com>
2022-10-03 18:30:09 +0200

    HomePage-html,url,app

--- HomePage/views.py
+++ HomePage/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def homePageView(request):
+    # HTML je uložené v HomePage/templates/HomePage/HomePage.html
+    return render(request,'HomePage/HomePage.html')
