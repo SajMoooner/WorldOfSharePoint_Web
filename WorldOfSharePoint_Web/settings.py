12139768ed2916d71efbf7c0cf88b17790d9103f
Simon <ochotnicky.simon1@gmail.com>
2022-12-26 14:45:39 +0100

    loginPage

--- WorldOfSharePoint_Web/settings.py
+++ WorldOfSharePoint_Web/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'loginPage.apps.LoginPageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
